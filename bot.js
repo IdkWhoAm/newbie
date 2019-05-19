c727e37df96b4ec0a206c97cc6dfd7899576f24c
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-19 09:08:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,24 +4,24 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-if (message.author.bot === true) return;
-	if (message.content === ";verify") {
+client.on('message', () => {
+	if (message.author.bot === true) return;
+	if (message.content === ":verify") {
 		message.delete()
 		if (!message.channel.name === "потверждение") return;
 		message.channel.send('Потверждаю...').then(msg => {
-			msg.delete(6)
+			msg.delete(1000)
 		})
 		let sor = message.guild.roles.find('name','непотвержден')
 		let ser = message.guild.roles.find('name','Игрок')
 		message.guild.member(message.author).removeRole(sor)
 		message.guild.member(message.author).addRole(ser).then(() => {
 			message.channel.send('Потверждены!').then(msg => {
-				msg.delete(4)
+				msg.delete(1000)
 			})
 		} ).catch(err => {
 			message.channel.send('Че-то пошло не так').then(msg => {
-				msg.delete(4)
+				msg.delete(2000)
 			})
 		})
 	}
